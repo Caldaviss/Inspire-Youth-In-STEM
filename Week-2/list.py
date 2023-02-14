9ea993b279914138171a500049ed54a29ef41611
Caldavis Irurah <caldavis118@gmail.com>
2023-02-14 10:46:14 +0300

    lists in python

---
+++ Week-2/list.py
@@ -0,0 +1,58 @@
+names=["Caldavis","John","Alex","Simon"]
+
+
+#Accessing items in a list
+print(names)
+
+
+print(names[0])
+
+print(names[-1])
+
+print(names[2])
+
+print(names[0:3])
+
+fruits=["mangoes","oranges","apples",]
+
+print(fruits[0])
+
+vegetables=["kales","spinach","cabbage","carrot"]
+
+stationery=["pencil","rubbber", "pen","ruler"]
+
+print(vegetables)
+print(stationery)
+
+shoppings=vegetables+stationery
+
+print(shoppings)
+
+for vegetable in vegetables:
+    print(vegetable)
+
+
+for shopping in shoppings:
+    print(shopping)
+
+print("My name is " + names [0] + " and my favorite fruit is " + fruits[1])
+
+
+
+
+# How to push work into github
+git add
+git status
+git commit -m "lists in python"
+git push
+
+
+
+
+
+
+
+
+
+
+
